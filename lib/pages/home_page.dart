8d907d3b453232b17a3e4fc00e469e9b25eecd67
turzo <ahsanulhasan3232@gmail.com>
2024-07-06 16:16:43 +0600

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,16 +19,20 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  /// A list to store fetched categories data.
   List<List<FetchProviderModel>> fetchList = [];
 
+  /// A list to store children categories of the selected category.
   List childrens = [];
 
   Future<List<FetchProviderModel>> getFetchList() async {
     final response =
         await http.get(Uri.parse('http://10.0.2.2:8000/api/v1/categories'));
     if (response.statusCode == 200) {
+      /// parsing the JSON response and returning a list of `FetchProviderModel` objects.
       return categoryFromJson(response.body);
     } else {
+      /// if status code is not 200 throws an exception
       throw Exception('Failed to load categories');
     }
   }
@@ -37,13 +41,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
+        /// `future` parameter takes the `getFetchList` function which fetches data.
         future: getFetchList(),
+
+        /// The `builder` function is called when the `Future` completes.
         builder: (context, AsyncSnapshot<List<FetchProviderModel>> snapshot) {
+          /// If snapshot doesn't have data, shows a loading spinner.
           if (!snapshot.hasData) {
             return const Center(
               child: CircularProgressIndicator(),
             );
           } else {
+            /// If snapshot has data, builds the UI.
             return SafeArea(
               child: Column(
                 children: [
@@ -56,22 +65,24 @@ class _HomePageState extends State<HomePage> {
                         return InkWell(
                           onTap: () {
                             setState(() {
-                              childrens = category.children;
+                              childrens = category.children ?? [];
                             });
                           },
                           child: Padding(
                             padding: const EdgeInsets.all(10),
                             child: Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-                                child: Center(
-                                    child: Text(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Center(
+                                child: Text(
                                   category!.name,
                                   style: const TextStyle(color: Colors.white),
-                                ))),
+                                ),
+                              ),
+                            ),
                           ),
                         );
                       },
